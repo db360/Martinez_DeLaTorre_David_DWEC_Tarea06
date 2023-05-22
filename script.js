4ef7acc88ee8db96debcfbe24af08aeab45ea83d
David Martinez <da.b@hotmail.es>
2023-05-22 15:31:41 +0200

    casi

--- script.js
+++ script.js
@@ -1,57 +1,196 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  $(document).ready(function () {
+    let apiKey = "bed732ac60af5977ac423955cd7a9225";
+    var localidadResponse;
 
-    $(document).ready(function() {
-        let apiKey = 'bed732ac60af5977ac423955cd7a9225';
+    async function mostrarTiempo() {
+      var localidad = $("#localidad").val();
+      var latResponse;
+      var lonResponse;
 
-        function mostrarTiempo() {
-            var localidad = $('#localidad').val();
+      try {
+        $("#resultado").empty();
+        // Primera llamada a la API para comprobar la localidad
 
-            $.ajax({
-                url: 'https://api.openweathermap.org/data/2.5/weather',
-                method: 'GET',
-                dataType: 'json',
-                data: {
-                    q: localidad,
-                    appid: apiKey,
-                    units: 'metric',
-                },
-                success: function(response) {
-                    console.log(response);
-                }
-            });
-        } // Función mostrarTiempo
-
-        function mostrarContaminacion() {
-
-            var localidad = $('#localidad').val();
-
-            $.ajax({
-                url: 'https://api.openweathermap.org/data/2.5/weather',
-                method: 'GET',
-                dataType: 'json',
-                data: {
-                    q: localidad,
-                    appid: apiKey,
-                    units: 'metric',
-                },
-                success: function(response) {
-
-                    // Datos a procesar: 
-                    console.log(response);
-                }
-            });
-        } // Función mostrarTiempo
-
-        $('#btnTiempo').click(function(evt) {
-            evt.preventDefault();
-            mostrarTiempo();
+        await $.ajax({
+          url: `http://api.openweathermap.org/geo/1.0/direct?q=${localidad}&limit=5&appid=${apiKey}`,
+          method: "GET",
+          dataType: "json",
+          success: function (response) {
+            localidadResponse = response[0].name;
+            latResponse = response[0].lat;
+            lonResponse = response[0].lon;
+            // console.log(latResponse);
+            // console.log(lonResponse);
+          },
+          // Si la petición falla
+          error: function (xhr, estado, error_localidad) {
+            console.log("Error producido: " + error_localidad);
+            console.log("Estado: " + estado);
+          },
+          //Tanto si falla como si funciona como sino funciona.
+          complete: function (xhr, estado) {
+            console.log("Petición Localidad Completa, estado: " + estado + xhr);
+          },
         });
 
-        $('#btnContaminacion').click(function(evt) {
-            evt.preventDefault();
-            mostrarContaminacion();
+        // Sunrise y sunset para el dia actual
+        await $.ajax({
+          url: "https://api.openweathermap.org/data/2.5/weather",
+          method: "GET",
+          dataType: "json",
+          data: {
+            lat: latResponse,
+            lon: lonResponse,
+            appid: apiKey,
+            units: "metric",
+            lang: "ES",
+          },
+          success: function (response) {},
+          // Si hay un error en la petición
+          error: function (xhr, estado, error_prevision) {
+            console.log("Error producido: " + error_prevision + xhr);
+            console.log("Estado: " + estado);
+          },
+          //Tanto si falla como si funciona como sino funciona.
+          complete: function (xhr, estado) {
+            console.log("Petición predicción completa, estado: " + estado);
+          },
         });
-    })
+        ///console.log(localidadResponse);
+        var fechaActual = new Date();
+        console.log(fechaActual);
+        // Segunda llamada a la API con la localidad de la primera llamada
+        await $.ajax({
+          url: `https://api.openweathermap.org/data/2.5/forecast`,
+          method: "GET",
+          dataType: "json",
+          data: {
+            lat: latResponse,
+            lon: lonResponse,
+            appid: apiKey,
+            units: "metric",
+            lang: "ES",
+          },
+          // Si se ha recibido una respuesta correctamente
+          success: function (response) {
+            var resultadoDiv = document.getElementById("resultado");
+            let diasFiltrados = new Array();
 
+            for (let index = 0; index < response.list.length; index++) {
+              var listaPrediccion = response.list[index];
+              var dt_txt = listaPrediccion.dt_txt;
+              //Filtramos las respuestas con hora 15:00
+              if (dt_txt.includes("15:00")) {
+                // Borramos el contenido anterior
+                diasFiltrados.push(listaPrediccion); // Agregamos el nuevo elemento
+              }
+            }
+            console.log(diasFiltrados);
 
+            // console.log(diasFiltrados)
+            // Crear el título
+            const tituloDiv = document.createElement("div");
+            tituloDiv.classList.add("titulo");
+            tituloDiv.innerHTML =
+              "<h2>Predicción para 5 días en " + localidadResponse + "</h2>";
+
+            resultadoDiv.appendChild(tituloDiv);
+
+            const cardsContainer = document.createElement("div");
+            cardsContainer.classList.add("cardsContainer");
+
+            for (const dia of diasFiltrados) {
+              var fecha = new Date(dia.dt * 1000);
+              var diaSemana = fecha.toLocaleDateString("es-ES", {
+                weekday: "long",
+              });
+              var hora = fecha.toLocaleTimeString("es-ES", {
+                hour: "numeric",
+                minute: "numeric",
+              });
+
+              var temperatura = dia.main.temp;
+              var viento = dia.wind.speed;
+              var estadoTiempo = dia.weather[0].main;
+              var descTiempo = dia.weather[0].description;
+              var icono = dia.weather[0].icon;
+              var lluvia;
+              if (dia.rain) {
+                lluvia = "Va a llover";
+              } else {
+                lluvia = "No va a llover";
+              }
+
+              var card = document.createElement("div");
+              card.classList.add("card");
+
+              var contenido = `
+                          <div class="cardTitle">${diaSemana} ${hora}</div>
+                          <img src="http://openweathermap.org/img/wn/${icono}.png" alt="Icono del tiempo">
+                          <div class="cardWeather">${estadoTiempo}</div>
+                          <div class="lluvia">${lluvia}</div>
+                          <div class="cardWeather">${descTiempo}</div>
+                          <div class="cardTemp">${temperatura}°C</div>
+                          <div class="cardWind">Viento: ${viento} m/s</div>
+                        `;
+              card.innerHTML = contenido;
+
+              cardsContainer.appendChild(card);
+            }
+
+            resultadoDiv.appendChild(cardsContainer);
+          },
+          // Si hay un error en la petición
+          error: function (xhr, estado, error_prevision) {
+            console.log("Error producido: " + error_prevision + xhr);
+            console.log("Estado: " + estado);
+          },
+          //Tanto si falla como si funciona como sino funciona.
+          complete: function (xhr, estado) {
+            console.log("Petición predicción completa, estado: " + estado);
+          },
+        });
+      } catch (error) {
+        console.log("Error producido: " + error);
+      }
+    } // FIN Funcion mostrar tiempo
+
+    /* Funcion para mostrar la contaminación*/
+    function mostrarContaminacion() {
+      $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather",
+        method: "GET",
+        dataType: "json",
+        data: {
+          q: localidadResponse,
+          appid: apiKey,
+          units: "metric",
+        },
+        // Si se ha recibido una respuesta correctamente
+        success: function (response) {
+          console.log(response);
+        },
+        error: function (xhr, estado, error_contaminacion) {
+          console.log("Error producido: " + error_producido);
+          console.log("Estado: " + estado);
+        },
+        //Tanto si falla como si funciona como sino funciona.
+        complete: function (xhr, estado) {
+          console.log("Petición completa, estado: " + estado);
+        },
+      });
+    } //FIN Función mostrarContaminacion
+
+    /* Listeners para los clicks de los botones*/
+    $("#btnTiempo").click(function (evt) {
+      evt.preventDefault();
+      mostrarTiempo();
+    });
+
+    $("#btnContaminacion").click(function (evt) {
+      evt.preventDefault();
+      mostrarContaminacion();
+    });
+  });
 });
